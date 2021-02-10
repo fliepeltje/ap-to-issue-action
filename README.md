b73a6147bf315ff16692573b8366ea20ccb1feee
fliepeltje <donatas@rasiukevicius.dev>
2021-02-10 10:26:37 +0100

    init

---
+++ README.md
@@ -0,0 +1,69 @@
+## Usage
+
+Create a workflow file in your .github/workflows directory as follows:
+
+### workflow.yaml
+
+
+    name: "Workflow"
+    on: ["push"]
+    jobs:
+      build:
+        runs-on: "ubuntu-latest"
+        steps:
+          - uses: "actions/checkout@master"
+          - name: "AP to Issue"
+            uses: "fliepeltje/ap-to-issue-action@v2.4.1"
+            id: "ap"
+            with:
+              TOKEN: ${{ secrets.GITHUB_TOKEN }}
+
+
+### Inputs
+
+| Input          | Default value                   | Description                                                                                                            |
+| -------------- | ------------------------------- | ---------------------------------------------------------------------------------------------------------------------- |
+| `REPO`         | `"${{ github.repository }}"`    | The path to the repository where the action will be used, e.g. 'alstr/my-repo' (automatically set)                     |
+| `BEFORE`       | `"${{ github.event.before }}"`  | The SHA of the last pushed commit (automatically set)                                                                  |
+| `SHA`          | `"${{ github.sha }}"`           | The SHA of the latest commit (automatically set)                                                                       |
+| `TOKEN`        | `"${{ secrets.GITHUB_TOKEN }}"` | The GitHub access token to allow us to retrieve, create and update issues (automatically set)                          |
+| `LOOKUP_TABLE` | No default value                | Used to configure lookup table for actual name to gh username, specify as kv pairs `donatas=fliepeltje niels=nielsbom` |
+
+## Examples
+
+### Adding APs
+
+When taking meeting notes, you can specify a point of action with the following syntax:
+
+```md
+AP Donatas: Do this important thing
+``` 
+
+This will result in creating a new issue for `fliepletje` with the title **Do this important thing**
+
+### Multiline APs
+
+If an action requires a body/description, you can specify this by using indentation. Tabs and spaces work - we don't judge:
+
+```md
+AP Donatas: Another imprtant task
+    This is a description
+```
+
+When specifying multiple action points, a new line is required:
+```md
+AP Donatas: Bla
+    description
+
+AP Donatas: More bla
+    more description
+```
+
+
+### Multiple assignees
+
+To specify multiple assignees to an issue, use the `|` operator:
+```md
+AP Donatas|Niels: Do this together
+```
+
---
+++ main.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+"""Convert meeting actions to GitHub issues."""
+
+import os
+import requests
+import re
+import json
+from dataclasses import dataclass
+from time import sleep
+
+BASE_URL = "https://api.github.com/repos/"
+REPO = os.environ.get("INPUT_REPO")
+TOKEN = os.environ.get("INPUT_TOKEN")
+PREV_COMMIT = os.environ.get("INPUT_BEFORE")
+SHA = os.environ.get("INPUT_SHA")
+LOOKUP_TABLE_STRING = os.environ.get("LOOKUP_TABLE")
+LOOKUP_TABLE_ITER = (x.partition("=") for x in LOOKUP_TABLE_STRING.split())
+LOOKUP_TABLE = {k.lower().strip(): v.strip() for k, _, v in LOOKUP_TABLE_ITER}
+
+
+@dataclass
+class Issue:
+    assignees: list[str]
+    title: str
+    description: str
+    high_priority: bool
+
+    @classmethod
+    def from_text(cls, issue_text: str) -> "Issue":
+        assignee_text, _, issue = issue_text.partition(":")
+        title, _, description = issue.partition("\n")
+        return cls(
+            assignees=[x.strip() for x in assignee_text.replace("!", "").split("|")],
+            title=title.strip(),
+            description=description.strip(),
+            high_priority="!" in assignee_text,
+        )
+
+
+def extract_actions(notes: str) -> list[str]:
+    return [s for s, _ in re.findall(r"AP\s((.|\n\t|\n\s\s\s\s)*)", notes)]
+
+
+def get_open_issues(page: int = 1) -> list[dict]:
+    issues_response = requests.get(
+        url=f"{BASE_URL}{REPO}/issues",
+        params={"per_page": 100, "page": page, "state": "open", "labels": "todo"},
+        headers={
+            "Content-Type": "application/json",
+            "Authorization": f"token {TOKEN}",
+        },
+    )
+    if "next" in issues_response.links:
+        return issues_response.json() + get_open_issues(page + 1)
+    else:
+        return issues_response.json()
+
+
+def get_diff() -> str:
+    response = requests.get(
+        f"{BASE_URL}{REPO}/compare/{PREV_COMMIT}...{SHA}",
+        headers={
+            "Content-Type": "application/json",
+            "Authorization": f"token {TOKEN}",
+        },
+    )
+    if response.status_code == 200:
+        return response.text
+    else:
+        raise ConnectionError()
+
+
+def create_issue(issue: Issue) -> None:
+    response = requests.post(
+        url=f"{BASE_URL}/issues",
+        headers={
+            "Content-Type": "application/json",
+            "Authorization": f"token {TOKEN}",
+        },
+        json={
+            "title": issue.title,
+            "body": issue.description,
+            "labels": ["todo"],
+            "assignees": [
+                LOOKUP_TABLE[name.lower().strip()] for name in issue.assignees
+            ],
+        },
+    )
+    if response.status_code == 201:
+        print(f"created issue: {issue}")
+    else:
+        print(f"failed to create issue: {issue}")
+
+    sleep(0.5)
+
+
+def main():
+    existing_issues = get_open_issues()
+    diff = get_diff()
+    issues = [Issue.from_text(issue_text) for issue_text in extract_actions(diff)]
+    existing_issue_titles = set(x["title"] for x in existing_issues)
+    to_create = (x for x in issues if x.title not in existing_issue_titles)
+    for new_issue in to_create:
+        create_issue(new_issue)
+
+
+if __name__ == "__main__":
+    main()
